e1a5f3c41544797d4debe7d5fd2bc910006aa73d
morenonaitana <morenonaitana@gmail.com>
2017-12-13 23:03:13 +0100

    some cleaning

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,16 @@
 // this is the main file that pulls in all other modules
 // you can require() bower components too!
-var example = require("./example");
 var giphy = require("./giphy");
 
-example.welcome();
-
 document.addEventListener('DOMContentLoaded', function () {
+    
+    // Create constants
     const btn = document.querySelector('#btnSubmit');
     const catInput = document.getElementById('catInput');
+    
+    // Fetch the cat action from the submit button click
     btn.addEventListener('click', () => {
         giphy.gifRequest(catInput.value);
     })
+
 })
